cba624143600fdbc403714a053300fae32f542b0
LeaCrazyBambi <lheynitz@gmail.com>
2021-03-30 15:41:48 +0200

    :)

---
+++ name.js
@@ -0,0 +1,22 @@
+const animals = "dolfin";
+
+console.log(animals);
+console.log(animals[1],animals[5]);
+
+const firstName = "Aurora";
+const age = "29";
+const year = "1993";
+console.log(firstName + " was " + age + " in " + year);
+
+const isMarried = true;
+console.log(firstName + " is married: " + isMarried);
+
+const programmingLanguage = "JavaScript";
+const isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+
+console.log(programmingLanguage[0], programmingLanguage[4]);
+
+console.log(programmingLanguage.length);
+
+
